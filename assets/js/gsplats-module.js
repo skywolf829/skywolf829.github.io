7edc1430e5792998911cedb7ca061cd15a1a0c2d
Skylar Wurster <swwurster@gmail.com>
2024-09-06 18:07:58 -0500

    1 3 2 rule them all

--- assets/js/gsplats-module.js
+++ assets/js/gsplats-module.js
@@ -1,2 +1,3 @@
 import * as GaussianSplats3D from '@mkkellogg/gaussian-splats-3d';
 window.GaussianSplats3D = GaussianSplats3D;
+window.THREE = GaussianSplats3D.THREE;
--- assets/js/three-module.js
+++
@@ -1,2 +0,0 @@
-import * as THREE from 'three';
-window.THREE = THREE;
