f6217940f8f01e1935207cdeec2ac5c1140dcaf1
Skylar Wurster <swwurster@gmail.com>
2024-09-07 08:48:52 -0500

    Url fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,6 +17,7 @@ const pages = document.querySelectorAll("[data-page]");
 // add event to all nav link
 for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
+    history.pushState(null, '', '/');
 
     for (let i = 0; i < pages.length; i++) {
       if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
@@ -29,6 +30,7 @@ for (let i = 0; i < navigationLinks.length; i++) {
         navigationLinks[i].classList.remove("active");
       }
     }
+    handleURL();
 
   });
 }
